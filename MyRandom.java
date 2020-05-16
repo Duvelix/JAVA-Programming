7c821bfae4762ed9ba87a193b1ea126ddde84a30
Duvelix <duvelix@keepmind.net>
2020-05-16 19:05:19 +0900

    Delete wrong files

--- MyRandom.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class MyRandom {
-
-    public static void main(String[] args){
-        Random number = new Random();
-        System.out.println(number.nextInt());
-    }
-}
